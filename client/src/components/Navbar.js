9948f971cefa776e297064dadf9d2b25c439cbc0
Dylan Solis <dylansolis@Dylans-MacBook-Air.local>
2024-08-02 17:35:25 -0600

    changed theme

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,30 +1,29 @@
+// src/components/Navbar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white topbar mb-4 static-top shadow">
-      <Link className="navbar-brand d-flex align-items-center justify-content-center" to="/">
-        <div className="navbar-brand-icon rotate-n-15">
-          <i className="fas fa-laugh-wink"></i>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand" to="/">MercadoTCG</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/iniciar-sesion">Iniciar Sesión</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/registrarse">Registrarse</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/crear-anuncio">Vender</Link>
+            </li>
+          </ul>
         </div>
-        <div className="navbar-brand-text mx-3">MercadoTCG</div>
-      </Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/iniciar-sesion">Iniciar Sesión</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/registrarse">Registrarse</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/crear-anuncio">Vender</Link>
-          </li>
-        </ul>
       </div>
     </nav>
   );
